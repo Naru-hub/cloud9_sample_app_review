0011978f21063ea4ee7c8ea540d311a021b22963
Naru-hub <1208ny0324@gmail.com>
2021-05-19 08:21:09 +0000

    [Finish]2-7

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '331222fefd0cf60d7abbd5a65c4540eadaf268577623f90a61b456ad107a892db47ffd72c3ccb9d33a2f8ff9164e570e2939ded955474ef442863d8c310874e2'
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_18_102155) do
+ActiveRecord::Schema.define(version: 2021_05_19_075825) do
 
   create_table "lists", force: :cascade do |t|
     t.string "title"
     t.string "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_id"
   end
 
 end
